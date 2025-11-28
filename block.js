f0e0024d453a0051f2ea820665b85c7aed6b4938
Hüseyin Atakan Kubat <atakan@Atakan-MacBook-Air-M2.local>
2025-11-28 14:36:24 +0300

    feat: Block sinifi ve SHA256 hash mekanizmasi olusturuldu

---
+++ block.js
@@ -0,0 +1,32 @@
+const crypto = require('crypto');
+
+class Block {
+    // Hem PoS hem PoW desteği var
+    constructor(index, timestamp, data, previousHash = '', validator = 'Sistem') {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.validator = validator; // Kim doğruladı?
+        this.nonce = 0;             // Madencilik sayısı
+        this.hash = this.calculateHash();
+    }
+
+    calculateHash() {
+        // Hash hesaplarken HEM validator ismini HEM de nonce değerini katıyoruz
+        const veriBirlestir = this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.validator + this.nonce;
+        return crypto.createHash('sha256').update(veriBirlestir).digest('hex');
+    }
+
+    // PoW için Madencilik Fonksiyonu
+    mineBlock(difficulty) {
+        // Hash istenen sayıda '0' ile başlayana kadar nonce'u artır
+        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+        console.log("⛏️  Blok Kazıldı (PoW): " + this.hash);
+    }
+}
+
+module.exports = Block;
